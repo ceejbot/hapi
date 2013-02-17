6fa9cb9dbc14f739c060a9138f602298f31f4027
C J Silverio <ceejceej@gmail.com>
2013-02-17 11:13:05 -0800

    Made hapi support asynchronous template engines like blade
    This required restructuring view Manager.render() and _get() to return
    their results asynchronously.
    
    Added two new flags to the views section of server options:
    asyncCompile -- set to true if the template engine compiles asynchronously
    asyncRender -- set to true if the template engine renders asynchronously
    
    Blade.js is an example of a template engine that does both asynchronously.
    jazz is an engine that compiles synchronously & renders async.
    
    To make blade layouts work, added a new option to the "views" options
    section. Set `layoutFile` to the name of the layout file you wish to
    use, with suffix appropriate for your layout engine. The default is
    "layout.html", so existing hapi layout code continues to work.
    
    This isn't generalized enough, since you'll want to be able to override
    layouts on the fly for some pages, but it's a start.
    
    Updated all the unit tests to get their results from render() in a
    callback instead of as a return value. I believe this is an
    implementation detail that shouldn't end up exposed to the application
    level, but I don't know the code base well enough to be sure yet.
    
    Added unit tests specific to the async template compilation & rendering
    code paths. 100% test coverage exists according to blanket & travis.
    
    Added blade to the dev dependencies.

--- test/unit/views.js
+++ test/unit/views.js
@@ -30,13 +30,34 @@ describe('Views', function () {
             path: viewsPath,
             layout: true
         });
-        
+
+        it('should handle omitting the options parameter', function (done) {
+
+            testView.render('valid/novars', {}, function (html) {
+                expect(html).to.exist;
+                expect(typeof html).equal('string');
+                done();
+            });
+
+        });
+
+        it('should handle omitting both context & options parameters', function (done) {
+
+            testView.render('valid/novars', function (html) {
+                expect(html).to.exist;
+                expect(typeof html).equal('string');
+                done();
+            });
+
+        });
+
         it('should work and not throw with valid (no layouts)', function (done) {
 
             var fn = (function () {
-                var html = testView.render('valid/test', { title: 'test', message: 'Hapi' });
-                expect(html).to.exist;
-                expect(html.length).above(1);
+                testView.render('valid/test', { title: 'test', message: 'Hapi' }, function (html) {
+                    expect(html).to.exist;
+                    expect(html.length).above(1);
+                });
             });
 
             expect(fn).to.not.throw();
@@ -46,21 +67,40 @@ describe('Views', function () {
         it('should work and not throw with valid (with layouts)', function (done) {
 
             var fn = (function () {
-                var html = testViewWithLayouts.render('valid/test', { title: 'test', message: 'Hapi' });
-                expect(html).to.exist;
-                expect(html.length).above(1);
+                testViewWithLayouts.render('valid/test', { title: 'test', message: 'Hapi' }, function (html) {
+                    expect(html).to.exist;
+                    expect(html.length).above(1);
+                });
             });
 
             expect(fn).to.not.throw();
             done();
         });
 
-        it('should throw when referencing non existant partial (with layouts)', function (done) {
+        it('should respond with an error when referencing a non-existent layout', function (done) {
+
+            var missingLayoutView = new Views({
+                path: viewsPath,
+                layout: true,
+                layoutFile: 'invalid/missing'
+            });
+
+            missingLayoutView.render('valid/test', { title: 'test', message: 'Hapi' }, function (result) {
+                expect(result).to.exist;
+                expect(typeof result).equal('object');
+                expect(result.hasOwnProperty('isBoom')).equal(true);
+                expect(result.isBoom).equal(true);
+                done();
+            });
+        });
+
+        it('should throw when referencing non-existent partial (with layouts)', function (done) {
 
             var fn = (function () {
-                var html = testViewWithLayouts.render('invalid/test', { title: 'test', message: 'Hapi' });
-                expect(html).to.exist;
-                expect(html.length).above(1);
+                testViewWithLayouts.render('invalid/test', { title: 'test', message: 'Hapi' }, function (html) {
+                    expect(html).to.exist;
+                    expect(html.length).above(1);
+                });
             });
 
             expect(fn).to.throw();
@@ -70,9 +110,10 @@ describe('Views', function () {
         it('should throw when referencing non existant partial (no layouts)', function (done) {
 
             var fn = (function () {
-                var html = testView.render('invalid/test', { title: 'test', message: 'Hapi' });
-                expect(html).to.exist;
-                expect(html.length).above(1);
+                testView.render('invalid/test', { title: 'test', message: 'Hapi' }, function (html) {
+                    expect(html).to.exist;
+                    expect(html.length).above(1);
+                });
             });
 
             expect(fn).to.throw();
@@ -84,18 +125,21 @@ describe('Views', function () {
             var fn = (function () {
                 var opts = { title: 'test', message: 'Hapi' };
                 opts[testView.options.layoutKeyword] = 1;
-                var html = testViewWithLayouts.render('valid/test', opts);
+                testViewWithLayouts.render('valid/test', opts, function (html) {
+                    // no-op
+                });
             });
 
             expect(fn).to.throw();
             done();
         });
 
-        it('should throw on compile error (invalid template code)', function (done) {
+        it('should call back with error on compile error (invalid template code)', function (done) {
 
-            var error = testView.render('invalid/badmustache', { title: 'test', message: 'Hapi' });
-            expect(error instanceof Error).to.equal(true);
-            done();
+            testView.render('invalid/badmustache', { title: 'test', message: 'Hapi' }, function (error) {
+                expect(error instanceof Error).to.equal(true);
+                done();
+            });
         });
 
         it('should load partials and be able to render them', function (done) {
@@ -109,15 +153,16 @@ describe('Views', function () {
                     }
                 });
 
-                var html = tempView.render('testPartials', {});
-                expect(html).to.exist;
-                expect(html.length).above(1);
+                tempView.render('testPartials', {}, function (html) {
+                    expect(html).to.exist;
+                    expect(html.length).above(1);
+                });
             });
 
             expect(fn).to.not.throw();
             done();
         });
-        
+
         it('should load partials and render them EVEN if viewsPath has trailing slash', function (done) {
 
             var fn = (function () {
@@ -129,15 +174,29 @@ describe('Views', function () {
                     }
                 });
 
-                var html = tempView.render('testPartials', {});
-                expect(html).to.exist;
-                expect(html.length).above(1);
+                tempView.render('testPartials', {}, function (html) {
+                    expect(html).to.exist;
+                    expect(html.length).above(1);
+                });
             });
 
             expect(fn).to.not.throw();
             done();
         });
-        
+
+        it('returns an error when given a non-string template name', function (done) {
+
+            testView.render(function () { return 'foo'; }, { title: 'test', message: 'Hapi' }, function (result) {
+
+                expect(result).to.exist;
+                expect(typeof result).equal('object');
+                expect(result.hasOwnProperty('isBoom')).equal(true);
+                expect(result.isBoom).equal(true);
+                done();
+            });
+
+        });
+
         it('should skip loading partial if engine does not have registerPartial method', function (done) {
 
             var fn = (function () {
@@ -152,14 +211,156 @@ describe('Views', function () {
                     }
                 });
 
-                var html = tempView.render('testPartials', {});
-                expect(html).to.exist;
-                expect(html.length).above(1);
-            })
+                tempView.render('testPartials', {}, function (html) {
+                    expect(html).to.exist;
+                    expect(html.length).above(1);
+                });
+            });
 
             expect(fn).to.not.throw();
             done();
-        })
+        });
+    });
+
+    describe('#renderAsync', function () {
+
+        var asyncView = new Views({
+            path: viewsPath,
+            layout: false,
+            engines: {
+                'blade': {
+                    module: 'blade',
+                    extension: 'blade'
+                }
+            },
+            asyncCompile: true,
+            asyncRender: true,
+            cache: false
+        });
+
+        var asyncViewWithLayouts = new Views({
+            path: viewsPath,
+            layout: true,
+            layoutKeyword: 'content',
+            layoutFile: 'blade/layout.blade',
+            engines: {
+                'blade': {
+                    module: 'blade',
+                    extension: 'blade'
+                }
+            },
+            asyncCompile: true,
+            asyncRender: true,
+            cache: false
+        });
+
+        it('can render a valid template without a layout', function (done) {
+
+            asyncView.render('blade/valid', { title: 'test', message: 'Hapi' }, function (result) {
+                expect(result).to.exist;
+                expect(typeof result).equal('string');
+                expect(result.length).above(1);
+                expect(result.indexOf('<title>test</title>')).above(-1);
+                done();
+            });
+        });
+
+        it('can render a valid template with a layout', function (done) {
+
+            asyncViewWithLayouts.render('blade/content', { title: 'test', message: 'Hapi' }, function (result) {
+                expect(result).to.exist;
+                expect(typeof result).equal('string');
+                expect(result.length).above(1);
+                expect(result.indexOf('<title>test</title>')).above(-1);
+                expect(result.indexOf('rendered with a layout')).above(-1);
+                done();
+            });
+        });
+
+        it('responds with an error when compiling an invalid template', function (done) {
+
+            asyncView.render('blade/uncompiled', { title: 'no compile' }, function (result) {
+                expect(result).to.exist;
+                expect(typeof result).equal('object');
+                expect(result.hasOwnProperty('isBoom')).equal(true);
+                expect(result.isBoom).equal(true);
+                done();
+            });
+        });
+
+        it('responds with an error when rendering fails', function (done) {
+
+            asyncView.render('blade/incorrect', { title: 'test', message: 'Hapi' }, function (result) {
+                expect(result).to.exist;
+                expect(typeof result).equal('object');
+                expect(result.hasOwnProperty('isBoom')).equal(true);
+                expect(result.isBoom).equal(true);
+                expect(result.hasOwnProperty('message')).equal(true);
+                expect(result.message.indexOf('undefinedVariable is not defined')).equal(0);
+                done();
+            });
+        });
+
+        it('responds with an error when exceptions are thrown during render', function (done) {
+
+            asyncView.renderAsync(null, null, {}, { title: 'test', message: 'Hapi', content: 'boom!' }, function (result) {
+                expect(result).to.exist;
+                expect(typeof result).equal('object');
+                expect(result.hasOwnProperty('isBoom')).equal(true);
+                expect(result.isBoom).equal(true);
+                done();
+            });
+        });
+
+        it('responds with an error when rendering the content template fails (with layouts)', function (done) {
+
+            asyncViewWithLayouts.render('blade/incorrect', { title: 'test', message: 'Hapi' }, function (result) {
+                expect(result).to.exist;
+                expect(typeof result).equal('object');
+                expect(result.hasOwnProperty('isBoom')).equal(true);
+                expect(result.isBoom).equal(true);
+                done();
+            });
+        });
+
+        it('responds with an error when the layout context key is used', function (done) {
+
+            asyncViewWithLayouts.render('blade/content', { title: 'test', message: 'Hapi', content: 'boom!' }, function (result) {
+                expect(result).to.exist;
+                expect(typeof result).equal('object');
+                expect(result.hasOwnProperty('isBoom')).equal(true);
+                expect(result.isBoom).equal(true);
+                done();
+            });
+        });
+
+        it('should respond with an error when referencing a non-existent layout', function (done) {
+
+            var missingLayoutView = new Views({
+                path: viewsPath,
+                layout: true,
+                layoutKeyword: 'content',
+                layoutFile: 'blade/missing.blade',
+                engines: {
+                    'blade': {
+                        module: 'blade',
+                        extension: 'blade'
+                    }
+                },
+                asyncCompile: true,
+                asyncRender: true,
+                cache: false
+            });
+
+            missingLayoutView.render('blade/content', { title: 'test', message: 'Hapi' }, function (result) {
+                expect(result).to.exist;
+                expect(typeof result).equal('object');
+                expect(result.hasOwnProperty('isBoom')).equal(true);
+                expect(result.isBoom).equal(true);
+                done();
+            });
+        });
+
     });
 
     describe('#handler', function () {
